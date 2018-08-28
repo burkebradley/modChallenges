7ef58483cf1469197ed2d6c94875f423dcd6b20b
burke.bradley <burke.bradley@MU-C1-04.mullen.pvt.k12.co.us>
2018-08-28 09:10:36 -0600

    finished first challenge

---
+++ modChallenges/src/modChallenges/modChallenges.java
@@ -0,0 +1,51 @@
+package modChallenges;
+
+import java.util.Scanner;
+public class modChallenges
+	{
+		public static void main(String[] args)
+		{
+		// leapYear ();
+		System.out.println();
+		fizzBuzz ();
+		}
+				public static void leapYear ()	
+				{
+						Scanner userInput = new Scanner (System.in);
+						System.out.println("Type in a year to see if it is a leap year");
+						int userYear = userInput.nextInt();
+							
+							if ((userYear%4 == 0)&&(userYear%100 == 0)&&(userYear%400 == 0))
+								{
+									System.out.println(userYear + " is a leap year.");
+								}
+							else
+								{
+									System.out.println(userYear + " is not a leap year.");
+								}
+				}		
+				public static void fizzBuzz ()
+				{
+							for (int i = 1; i <= 100; i++)
+								{
+									System.out.println(i);
+									if ((i%3 == 0)&&(i%5 == 0))
+										{
+											
+											System.out.println("FizzBuzz");
+										}
+									else if(i%3 == 0) 
+										{
+											System.out.println("Fizz");
+										}
+									else if (i%5 == 0)
+										{
+											System.out.println("Buzz");
+										} 
+								}
+				}		
+		
+	}
+				
+
+	
