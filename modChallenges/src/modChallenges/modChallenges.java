ffcc0c73d400541ee07e3dfec97bd437e8659aa2
burke.bradley <burke.bradley@MU-C1-04.mullen.pvt.k12.co.us>
2018-08-30 08:50:23 -0600

    Must finish FizzBuzz

--- modChallenges/src/modChallenges/modChallenges.java
+++ modChallenges/src/modChallenges/modChallenges.java
@@ -30,8 +30,7 @@ public class modChallenges
 								{
 									System.out.println(i);
 									if ((i%3 == 0)&&(i%5 == 0))
-										{
-											
+										{	
 											System.out.println("FizzBuzz");
 										}
 									else if(i%3 == 0) 
@@ -42,6 +41,10 @@ public class modChallenges
 										{
 											System.out.println("Buzz");
 										} 
+									else 
+										{
+											System.out.println(i);
+										}
 								}
 				}		
 		
